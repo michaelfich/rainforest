195ed3dfecb8f6a7920f0c4da134e0e6bee356cf
Michael Fich <michaeltf@gmail.com>
2015-06-30 15:18:01 -0400

    Added validations for creating and updating a Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price_in_cents, presence: true, numericality: {only_integer: true}
+
   def price
     price_in_cents.to_f / 100
   end
