f5629609f84f1ae6ded2f5050c15e2b2bf0c46d0
Michael Fich <michaeltf@gmail.com>
2015-06-30 15:44:59 -0400

    Added actions for creating a new user in the database, and added functionality in the controller, model and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  # get 'products/index'
-  # get 'products/show'
-  # get 'products/new'
-  # get 'products/edit'
-
-  # get 'products/index' => 'products#index', as: :products
-  # get 'products/new' => 'products#new', as: :new_product
-  # get 'products/:id/edit' => 'products#edit', as: :edit_product
-  # get 'products/:id' => 'products#show', as: :product
-
   resources :products
+  resources :users, only: [:new, :create]
 end
